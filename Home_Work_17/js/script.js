593f48bd65d38382771ae2b71d2b1bb989ca1d54
reema rahhal <reema.rahhal96@gmail.com>
2022-04-06 12:28:16 +0300

    validation email

---
+++ Home_Work_17/js/script.js
@@ -0,0 +1,11 @@
+function validEmail() { 
+    var checkemail= (document.getElementById("email")).value;
+    const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(pattern.test(String(checkemail).toLowerCase())) {
+        alert("True , your email is Valid");
+    } 
+    else{
+        alert("False, your email isn't valid !");
+    } 
+    
+} 
