fdb23bed4712c3a2e8ae9c49db8e18f5163ca4a7
reema rahhal <reema.rahhal96@gmail.com>
2022-04-06 12:50:08 +0300

    Valid email

--- Home_Work_17/js/script.js
+++ Home_Work_17/js/script.js
@@ -1,4 +1,4 @@
-function validEmail() { 
+ function validEmail() { 
     var checkemail= (document.getElementById("email")).value;
     const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if(pattern.test(String(checkemail).toLowerCase())) {
@@ -8,4 +8,4 @@ function validEmail() {
         alert("False, your email isn't valid !");
     } 
     
-} 
+}  
