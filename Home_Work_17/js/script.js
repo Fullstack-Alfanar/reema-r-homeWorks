b7525290b42795fa23db454adc63260596c2b5b3
reema rahhal <reema.rahhal96@gmail.com>
2022-04-16 16:49:42 +0300

    update valid email function

--- Home_Work_17/js/script.js
+++ Home_Work_17/js/script.js
@@ -1,13 +1,13 @@
  function CheckValidEmail(email) { 
-    var checkEmail= document.getElementById("email");
-    var pattern = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;;
-    if(!pattern.test(checkEmail.value)) { 
-        alert("False, your email isn't valid !"); 
+    var email= document.getElementById("email");
+    var pattern = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if(!pattern.test(email.value)) { 
+      alert("False, your email isn't valid !");
         return false;
 
     } 
-    else{ 
-        alert("True , your email is Valid"); 
+    else{  
+        alert("True , your email is Valid");
         return true;
 
     } 
