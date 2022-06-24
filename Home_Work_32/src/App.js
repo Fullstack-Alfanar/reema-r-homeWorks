af6d1a733e8f4393017c894e9dd3cd6173ce42c4
reema rahhal <reema.rahhal96@gmail.com>
2022-06-24 21:11:06 +0300

    update

---
+++ Home_Work_32/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import ChangColor from './components/Change-Color/color';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <ChangColor/>
+
+
+      
+      </header>
+
+      
+    </div>
+  );
+}
+
+export default App;
